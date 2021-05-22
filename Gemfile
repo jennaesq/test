5d426a0c3c4c1caa104caa2ce91c5ae56ce1b013
Jenna Esquivel <jennaesq@AdminisatorsAir.hsd1.or.comcast.net>
2021-05-21 17:17:23 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Gemfile
+source "https://rubygems.org"
+gem "sinatra"
+gem "pg"    
+gem "rake"  # so we can run Rake tasks
+gem "sinatra-activerecord"    # for Active Record models
---
+++ app.rb
@@ -0,0 +1,20 @@
+## App.
+# 
+# Usage:
+# $ sudo gem install sinatra
+# $ ruby app.rb
+# $ open http://0.0.0.0:4567/
+ 
+require 'sinatra'
+
+#get '/' do
+#  'Welcome to Evidation (hello world)!'
+#end
+
+require "sinatra"
+
+class App < Sinatra::Base
+  get "/" do
+    erb :index
+  end
+end
